5e06e286fb274f3e5efc878a325a0487b7ded808
정민지 <s2018s38@e-mirim.hs.kr>
2021-10-30 15:28:22 +0900

    상단 툴바 레이아웃 잡기

--- app/src/main/java/com/github/sookhee/shinhanesgmarket/mypage/MypageFragment.kt
+++ app/src/main/java/com/github/sookhee/shinhanesgmarket/mypage/MypageFragment.kt
@@ -5,17 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.github.sookhee.shinhanesgmarket.R
+import android.widget.Toast
+import com.github.sookhee.shinhanesgmarket.databinding.FragmentMypageBinding
 
 class MypageFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentMypageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+    ): View {
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
+
+        setOnClickListener()
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setOnClickListener() {
+        binding.btnSetting.setOnClickListener { Toast.makeText(context, "SETTING", Toast.LENGTH_SHORT).show() }
     }
 }
