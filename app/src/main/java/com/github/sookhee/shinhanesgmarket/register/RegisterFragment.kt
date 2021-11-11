c102f8703f172d8f033430e8bb314c051da21444
정민지 <s2018s38@e-mirim.hs.kr>
2021-11-12 00:36:06 +0900

    상품 등록 버튼 눌렀을 때 빈 항목 체크하는 로직 추가

--- app/src/main/java/com/github/sookhee/shinhanesgmarket/register/RegisterFragment.kt
+++ app/src/main/java/com/github/sookhee/shinhanesgmarket/register/RegisterFragment.kt
@@ -1,10 +1,13 @@
 package com.github.sookhee.shinhanesgmarket.register
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.github.sookhee.shinhanesgmarket.R
 import com.github.sookhee.shinhanesgmarket.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
@@ -28,6 +31,39 @@ class RegisterFragment : Fragment() {
     }
 
     private fun setOnClickListener() {
+        binding.btnSubmit.setOnClickListener {
+            val title = binding.titleEditText.text
+            val category: Int? = 0
+            val price = binding.priceEditText.text
+            val content = binding.mainEditText.text
 
+            val needItem = mutableListOf<String>()
+            if (title.isEmpty()) {
+                needItem.add("제목")
+            }
+            if (category == null) {
+                needItem.add("카테고리")
+            }
+            if (price.isEmpty()) {
+                needItem.add("가격")
+            }
+            if (content.isEmpty()) {
+                needItem.add("내용")
+            }
+
+            if (needItem.size == 0) {
+                Toast.makeText(
+                    context,
+                    resources.getString(R.string.string_submit_success),
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    context,
+                    resources.getString(R.string.hint_need_input, TextUtils.join(", ", needItem)),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
