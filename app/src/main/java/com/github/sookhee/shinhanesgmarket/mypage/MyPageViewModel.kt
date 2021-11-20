cbc30b12fbcc7cfbc487795f417b61d81b0c13d5
정민지 <s2018s38@e-mirim.hs.kr>
2021-11-20 19:48:08 +0900

    관심 목록 파이어베이스 임시 연결

--- app/src/main/java/com/github/sookhee/shinhanesgmarket/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/github/sookhee/shinhanesgmarket/mypage/MyPageViewModel.kt
@@ -4,28 +4,44 @@ import android.util.Log
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.github.sookhee.domain.usecase.GetMyProductListUseCase
 import kotlinx.coroutines.Dispatchers
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.sookhee.domain.entity.Product
+import com.github.sookhee.domain.usecase.GetProductListWithQueryUseCase
 import kotlinx.coroutines.launch
 
 class MyPageViewModel @ViewModelInject constructor(
-    private val getMyProductListUseCase: GetMyProductListUseCase
+    private val getProductListWithQueryUseCase: GetProductListWithQueryUseCase
 ): ViewModel() {
     private val _myProductList = MutableLiveData<List<Product>>()
     val myProductList: LiveData<List<Product>>
         get() = _myProductList
 
+    private val _likeProductList = MutableLiveData<List<Product>>()
+    val likeProductList: LiveData<List<Product>>
+        get() = _likeProductList
+
     fun getMyProductList() {
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                val result = getMyProductListUseCase("feed_owner_id", "21200203")
+                val result = getProductListWithQueryUseCase("feed_owner_id", "21200203")
                 _myProductList.postValue(result)
 
             } catch (e: Exception) {
-                Log.i(TAG, "getCategoryList Exception: $e")
+                Log.i(TAG, "getMyProductList Exception: $e")
+            }
+        }
+    }
+
+    fun getLikeProductList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = getProductListWithQueryUseCase("feed_owner_id", "21200204")
+                _likeProductList.postValue(result)
+
+            } catch (e: Exception) {
+                Log.i(TAG, "getLikeProductList Exception: $e")
             }
         }
     }
