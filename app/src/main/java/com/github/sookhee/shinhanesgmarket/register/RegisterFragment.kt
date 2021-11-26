602ef6be187c0aab2569ff1b5913e241b956970c
정민지 <s2018s38@e-mirim.hs.kr>
2021-11-26 23:50:04 +0900

    이미지 여러장 업로드 기능 추가
    TODO
    - 이름 랜덤으로 생성
    - 레이어 분리

--- app/src/main/java/com/github/sookhee/shinhanesgmarket/register/RegisterFragment.kt
+++ app/src/main/java/com/github/sookhee/shinhanesgmarket/register/RegisterFragment.kt
@@ -1,16 +1,23 @@
 package com.github.sookhee.shinhanesgmarket.register
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.github.sookhee.shinhanesgmarket.R
 import com.github.sookhee.shinhanesgmarket.databinding.FragmentRegisterBinding
+import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,6 +26,8 @@ class RegisterFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var viewModel: RegisterViewModel
+
+    private val photoList = hashMapOf<String, String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +46,18 @@ class RegisterFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == CODE_SELECT_IMAGE) {
+            for (i in 0 until data?.clipData?.itemCount!!) {
+                photoList["fileName_$i.png"] = "${data.clipData?.getItemAt(i)?.uri}"
+            }
+
+            uploadImage()
+        }
     }
 
     @SuppressLint("SimpleDateFormat")
@@ -77,5 +98,41 @@ class RegisterFragment : Fragment() {
                 ).show()
             }
         }
+
+        binding.btnAddPhoto.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_PICK).apply {
+                type = MediaStore.Images.Media.CONTENT_TYPE
+                data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            }
+
+            startActivityForResult(galleryIntent, CODE_SELECT_IMAGE)
+        }
+    }
+
+    private fun uploadImage() {
+        val storage = FirebaseStorage.getInstance()
+
+        photoList.forEach { photo ->
+            val riverRef = storage.reference.child("/product_image/${photo.key}")
+
+            riverRef.putFile(photo.value.toUri())
+                .addOnFailureListener { Log.i("민지", "FAIL: $it") }
+                .addOnSuccessListener {
+                    Log.i("민지", "SUCCESS : ${photo.key}")
+
+                    storage.getReference(photo.key).downloadUrl.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Glide.with(this)
+                                .load(task.result)
+                                .into(binding.photoIcon)
+                        }
+                    }
+                }
+        }
+    }
+
+    companion object {
+        private const val CODE_SELECT_IMAGE = 10001
     }
 }
