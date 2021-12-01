2936f0ad30eed20678829c0a1886e434bc95b252
정민지 <s2018s38@e-mirim.hs.kr>
2021-12-02 01:36:01 +0900

    사용자 정보(loginInfo)를 static으로 가질 수 있도록 코드 추가

--- app/src/main/java/com/github/sookhee/shinhanesgmarket/AppApplication.kt
+++ app/src/main/java/com/github/sookhee/shinhanesgmarket/AppApplication.kt
@@ -1,8 +1,28 @@
 package com.github.sookhee.shinhanesgmarket
 
 import android.app.Application
+import com.github.sookhee.domain.entity.User
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AppApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    fun setLoginInfo(user: User) {
+        loginInfo = user
+    }
+
+    fun getLoginInfo(): User {
+        return loginInfo
+    }
+
+    companion object {
+        private lateinit var instance: AppApplication
+        fun getInstance(): AppApplication = instance
+
+        private var loginInfo = User()
+    }
 }
