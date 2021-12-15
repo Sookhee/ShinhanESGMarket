e262b86a26bf4b62298b15c3fd946731b17c131d
정민지 <s2018s38@e-mirim.hs.kr>
2021-12-16 00:08:15 +0900

    스플래쉬 스크린 딜레이 1.5초

--- app/src/main/java/com/github/sookhee/shinhanesgmarket/splash/SplashActivity.kt
+++ app/src/main/java/com/github/sookhee/shinhanesgmarket/splash/SplashActivity.kt
@@ -3,19 +3,15 @@ package com.github.sookhee.shinhanesgmarket.splash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import android.view.ViewPropertyAnimator
 import android.widget.ImageView
 import androidx.lifecycle.ViewModelProvider
 import com.github.sookhee.shinhanesgmarket.AppApplication
-import com.github.sookhee.shinhanesgmarket.MainActivity
 import com.github.sookhee.shinhanesgmarket.R
 import com.github.sookhee.shinhanesgmarket.user.UserLoginActivity
 import com.github.sookhee.shinhanesgmarket.user.UserViewModel
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay as delay1
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -55,8 +51,10 @@ class SplashActivity : AppCompatActivity() {
 //            startActivity(loginIntent)
 //        }
 
-        loginIntent = Intent(this, UserLoginActivity::class.java)
-        startActivity(loginIntent)
+        Handler().postDelayed({
+            loginIntent = Intent(this, UserLoginActivity::class.java)
+            startActivity(loginIntent)
+        }, 1500)
     }
 
     private fun setObserver() {
